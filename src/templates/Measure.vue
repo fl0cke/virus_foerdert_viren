196149c3774d0b90d2a7e18bca13519a8563ca33
Christian <p-git@c5s.de>
2020-03-22 13:50:30 +0100

    fixed typo

--- src/templates/Measure.vue
+++ src/templates/Measure.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="mt-2 flex justify-between items-end content-end">
       <h5 class="font-semibold">Maßnahme Nr. {{$page.measure.number}}</h5>
-      <g-link to="/massnahmen" class="link">Zur übersicht</g-link>
+      <g-link to="/massnahmen" class="link">Zur Übersicht</g-link>
     </div>
     <div class="markdown mt-4" v-html="$page.measure.content" />
   </Layout>

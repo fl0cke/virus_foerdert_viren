cee7f25184c44918c54bf658675c2c241c92bcd7
Christian <p-git@c5s.de>
2020-03-22 19:58:18 +0100

    Content for about us page

--- src/pages/UeberUns.vue
+++ src/pages/UeberUns.vue
@@ -2,7 +2,12 @@
   <Layout>
     <h1 class="font-bold text-3xl md:text-5xl text-center mt-8 leading-snug">
       Über uns</h1>
-    <p class="mt-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      
+      <p class="mt-8">In den letzten Tagen wurden viele Arbeitsplätze in das Home-Of­fice verlegt. Dabei sind oftmals die Sicherheitsaspekte vernachlässigt worden. Diese Situation nutzen Cyberkriminelle. Über oft nicht ausreichend gesicherte virtuelle Arbeitsumgebungen können sie durch Schadsoftware an sensible Informationen erlangen.</p>
+
+<p class="mt-8">Dabei ist es für die Arbeitnehmer:innen und Arbeitgeber:innen nicht leicht, den Überblick über die relevanten Sicherheitsanforderungen zu behalten, die bei der Einrichtung eines Home-Of­fice wichtig sind. Aus diesem Grund haben wir eine interaktive Checkliste erstellt, die dem Nutzer die grundlegenden Sicherheitskriterien näher erläutert. So gibt es einen roten Faden, an dem sie sich bei der Einrichtung der Home-Of­fice Arbeitsplätze orientieren können.</p>
+
+    <p class="mt-8">Dieses Projekt ist im Rahmen des <a href="https://wirvsvirushackathon.org/">#WirVsVirus Hackathons</a> entstanden. Innerhalb von 48 Stunden haben wir uns als Team von Freiwilligen zusammengefunden und gemeinsam versucht unseren Beitrag zur Lösung von Herausforderungen, die durch die Corona-Krise entstanden sind zu leisten.</p>
   </Layout>
 </template>
 

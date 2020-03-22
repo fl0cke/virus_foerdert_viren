60ff2ac9625b5e9fc7373f1df3beadb1fee0d131
Christian <p-git@c5s.de>
2020-03-22 20:56:39 +0100

    impressum

--- src/pages/Impressum.vue
+++ src/pages/Impressum.vue
@@ -2,21 +2,48 @@
   <Layout>
     <h1 class="font-bold text-3xl md:text-5xl text-center mt-8 leading-snug">
       Impressum</h1>
-      
-      <h2>Angaben gemäß § 5 TMG:</h2>
-      <p>Virus förder Viren<br>Max Mustermann</p>
-      <h3>Postanschrift:</h3>
-      <p>a<br></p>
-      <h3>Kontakt:</h3>
-      <p>Telefon: <br>E-Mail: </p>
-      <p></p><p><br></p>
-      <p></p><h2>Hinweise zur Website</h2>
-      <p></p><h3>Verantwortlich für journalistisch-redaktionelle Inhalte ist:</h3>
-      <p>Max Mustermann<br></p>
-      <p></p><h2>Information gemäß § 36 VSBG</h2>
-      <p>Gemäß § 36 VSBG (Verbraucherstreitbeilegungsgesetz – Gesetz über die alternative Streitbeilegung in Verbrauchersachen) erklärt der Betreiber dieser Website:</p>
-      <p>Wir sind weder bereit noch verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-      <p></p><p><em>Das Impressum wurde mit dem <a href="https://www.activemind.de/datenschutz/impressums-generator/">Impressums-Generator der activeMind AG</a> erstellt.</em></p>
+
+      <h2>Angaben gemäß § 5 TMG</h2>
+
+      <p> Virus fördert Viren - IT-Sicherheit im Home-Office <br>
+      Max Mustermann<br>
+      Musterstadt</p>
+
+
+      <h2>Kontakt</h2>
+
+      <p>Telefon: <a href="tel:+"></a><br>
+      E-Mail: <a href="mailto:">a@test.de</a></p>
+
+      <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+
+      <p>Max Mustermann<br>Musterstadt</p>
+
+
+      <h2>Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h2>
+
+      <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+      <h2>Haftung für Inhalte</h2>
+
+      <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+
+      <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+      <h2>Haftung für Links</h2>
+
+      <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
+
+      <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+      <h2>Urheberrecht</h2>
+
+      <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+
+      <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+      <p>Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
+
 
   </Layout>
 </template>
@@ -34,6 +61,10 @@ h2 {
   margin-top: 2rem;
   font-weight: bold;
   font-size: 1.5rem;
+}
+
+h3 {
+  font-weight: bold;
 }
 
 ul {
